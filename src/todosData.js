6bbc007c1bd34e94a7996a773ea7609fab3636e9
Mereilim <ameekah93@gmail.com>
2019-11-21 15:01:21 -0500

    TodoReact-freeCodeCamp

---
+++ src/todosData.js
@@ -0,0 +1,44 @@
+const todosData=[
+
+
+{
+id:1,
+text: "Take out Trash",
+completed: true
+},
+
+{
+    id:1,
+    text: "Take out Trash",
+    completed: true
+    },
+    {
+        id:2,
+        text: "Dinner with wife",
+        completed: true
+        },
+        {
+            id:3,
+            text: "Do smth",
+            completed: false
+            },
+    
+            {
+                id:4,
+                text: "Do Laundry",
+                completed: true
+                },
+        
+
+
+
+
+
+
+
+
+
+
+]
+
+export default todosData;
